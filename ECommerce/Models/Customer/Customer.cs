e8ffcfb6eb216885db92cce0b6a4b5181df8c06e
Mohammad Oumeir Rengony <oumeir.rengony@hangarww.com>
2021-04-09 15:53:14 +0400

    Add project files.

---
+++ ECommerce/Models/Customer/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ECommerce.Models.Customer
+{
+    public class Customer:IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
